e2cfc3002d1a28dd7cbc5cc866e2ad1182c2a928
HawaOwO <dayinialhawani@student.usm.my>
2023-06-26 13:57:24 +0800

    add page login and register

--- app/src/main/java/com/example/dentalpro/MainActivity.java
+++ app/src/main/java/com/example/dentalpro/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.dentalpro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public Button FirstBtn, SecondBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirstBtn = (Button) findViewById(R.id.button1);
+        FirstBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            }
+        });
+        SecondBtn = (Button) findViewById(R.id.button2);
+        SecondBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            }
+        });
     }
 }
