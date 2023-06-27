e7dccde6f487053eb09a4df99bc5eafa72e2de12
HawaOwO <dayinialhawani@student.usm.my>
2023-06-27 13:58:48 +0800

    add login by gmail account

--- app/src/main/java/com/example/dentalpro/Login.java
+++ app/src/main/java/com/example/dentalpro/Login.java
@@ -1,14 +1,153 @@
 package com.example.dentalpro;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
 
 public class Login extends AppCompatActivity {
+    EditText userEmail, userPw;
+    Button LoginBtn;
+    ProgressBar progressBar;
+    FirebaseAuth fAuth;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    ImageView google_img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userEmail = findViewById(R.id.Email);
+        userPw = findViewById(R.id.Password);
+        progressBar = findViewById(R.id.progressBar2);
+        fAuth = FirebaseAuth.getInstance();
+        LoginBtn = findViewById(R.id.button);
+
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = userEmail.getText().toString().trim();
+                String password = userPw.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    userEmail.setError("Email is required");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    userPw.setError("Password is required");
+                    return;
+                }
+
+                if (password.length() < 8) {
+                    userPw.setError("Password must be over than 8 characters");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                // authenticate the user
+
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Login.this, "Welcome back user", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity2.class));
+                        } else {
+                            Toast.makeText(Login.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+
+                    }
+                });
+
+            }
+        });
+
+        google_img = findViewById(R.id.google);
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        gsc = GoogleSignIn.getClient(this, gso);
+        google_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignInWithGoogle();
+            }
+        });
     }
+    private void SignInWithGoogle() {
+        Intent intent = gsc.getSignInIntent();
+        startActivityForResult(intent, 100);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==100) {
+           Task<GoogleSignInAccount> task=GoogleSignIn.getSignedInAccountFromIntent(data);
+           try {
+               //task.getResult(ApiException.class);
+//               HomeActivity();
+               //Toast.makeText(Login.this, "Welcome back user", Toast.LENGTH_SHORT).show();
+               //startActivity(new Intent(getApplicationContext(), MainActivity2.class));
+               GoogleSignInAccount account = task.getResult(ApiException.class);
+               firebaseAuthWithGoogle(account);
+           } catch (ApiException e) {
+               //Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+               Toast.makeText(Login.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+               progressBar.setVisibility(View.GONE);
+           }
+        }
+
+    }
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        fAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = fAuth.getCurrentUser();
+                            // Continue with your app logic or redirect to the main activity
+                            Toast.makeText(Login.this, "Welcome back user", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity2.class));
+                        } else {
+                            Toast.makeText(Login.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
 }
--- app/src/main/java/com/example/dentalpro/SignUp.java
+++ app/src/main/java/com/example/dentalpro/SignUp.java
@@ -1,14 +1,95 @@
 package com.example.dentalpro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity {
+
+    EditText userFName, userLName ,userEmail,userPw,userPhone, userPwConfirm;
+    Button SignUpBtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        userFName     = findViewById(R.id.Fname);
+        userLName     = findViewById(R.id.Lname);
+        userEmail     = findViewById(R.id.EmailSignup);
+        userPw        = findViewById(R.id.PwSignup);
+        userPwConfirm = findViewById(R.id.ConfirmPwSignup);
+        userPhone     = findViewById(R.id.Mnumber);
+        SignUpBtn     = findViewById(R.id.Signupbutton);
+        progressBar   = findViewById(R.id.progressBar3);
+
+        fAuth = FirebaseAuth.getInstance();
+
+//        if(fAuth.getCurrentUser() != null){
+//            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+//            finish();
+//        }
+
+        SignUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = userEmail.getText().toString().trim();
+                String password = userPw.getText().toString().trim();
+                String passwordC = userPwConfirm.getText().toString().trim();
+                final String firstName = userFName.getText().toString();
+                final String lastName = userLName.getText().toString();
+                final String phone    = userPhone.getText().toString();
+
+                if(TextUtils.isEmpty(email)){
+                    userEmail.setError("Email is required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    userPw.setError("Password is required");
+                    return;
+                }
+
+                if(password.length() < 8){
+                    userPw.setError("Password must be more than 8 characters");
+                    return;
+                }
+                if(!password.equals(passwordC)){
+                    userPwConfirm.setError("Password must be the same");
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+                // register the user in firebase
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(SignUp.this, "Account is created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity2.class));
+                        }else {
+                            Toast.makeText(SignUp.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            //progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+            }
+        });
+
     }
+
 }
