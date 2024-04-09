2c91d78fae723926f7ada293e117438d9b3d886a
HawaOwO <dayinialhawani@student.usm.my>
2024-04-09 16:19:48 +0800

    add dashboard and change icon, next to do is add download button for analytics purpose

---
+++ app/src/main/java/com/example/dentalpro/mainpagedashboard.java
@@ -0,0 +1,88 @@
+package com.example.dentalpro;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+
+
+public class mainpagedashboard extends Fragment {
+
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_mainpagedashboard, container, false);
+
+        Button homeButton = rootView.findViewById(R.id.home);
+        Button scannerButton = rootView.findViewById(R.id.scanner);
+        Button issueButton = rootView.findViewById(R.id.feedback);
+        Button profileButton = rootView.findViewById(R.id.profile);
+
+        ImageSlider imageSlider = rootView.findViewById(R.id.imageSlider);
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
+
+        slideModels.add(new SlideModel(R.drawable.image1, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.image2, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.image3, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.image4, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.image5, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.image6, ScaleTypes.FIT));
+
+        imageSlider.setImageList(slideModels, ScaleTypes.FIT);
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Replace the current fragment with the fragmentHome
+                replaceFragment(new fragmentHome());
+            }
+        });
+
+        scannerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Replace the current fragment with the fragmentScanner
+                replaceFragment(new fragmentScanner());
+            }
+        });
+
+        issueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Replace the current fragment with the fragmentIssue
+                replaceFragment(new fragmentIssue());
+            }
+        });
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Replace the current fragment with the fragmentProfile
+                replaceFragment(new fragmentProfile());
+            }
+        });
+
+        return rootView;
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame1, fragment);
+        fragmentTransaction.commit();
+    }
+
+
+}
